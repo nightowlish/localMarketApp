16975557489a91af43baa9c3ffa83313ff8e54d1
nightowlish <oanaolf@gmail.com>
2020-05-10 21:52:47 +0300

    ownproduct adapter for seller's products

--- app/src/main/java/local/market/app/data/OwnProductAdapter.java
+++ app/src/main/java/local/market/app/data/OwnProductAdapter.java
@@ -1,4 +1,51 @@
 package local.market.app.data;
 
-public class OwnProductAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import local.market.app.R;
+
+public class OwnProductAdapter extends ArrayAdapter<Product> {
+    Product currentProduct;
+    Button delete;
+    TextView name, price;
+    ImageView image;
+    public OwnProductAdapter(Context context, ArrayList<Product> products) {
+        super(context, 0, products);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Product product = getItem(position);
+        currentProduct = product;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_layout, parent, false);
+        }
+
+        name = (TextView) convertView.findViewById(R.id.textView14);
+        price = (TextView) convertView.findViewById(R.id.textView12);
+        image = (ImageView) convertView.findViewById(R.id.imageView);
+        delete = (Button) convertView.findViewById(R.id.button12);
+
+        name.setText(product.getName());
+        price.setText(String.valueOf(product.getPrice()));
+        image.setImageBitmap(product.getImage());
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteProduct();
+            }
+        });
+        return convertView;
+    }
+    public void deleteProduct() {
+
+    }
 }
--- app/src/main/java/local/market/app/data/SellActivity.java
+++ app/src/main/java/local/market/app/data/SellActivity.java
@@ -2,7 +2,13 @@ package local.market.app.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import local.market.app.R;
 
@@ -12,5 +18,22 @@ public class SellActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+
+
+        Button addProduct = (Button) findViewById(R.id.button13);
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SellActivity.this, AddProductActivity.class);
+                SellActivity.this.startActivity(myIntent);
+            }
+        });
+
+        ListView listview = (ListView) findViewById(R.id.listView7);
+        int category = Integer.parseInt(getIntent().getStringExtra("category"));
+        ArrayList<Product> products = Data.getCategoryProducts(category);
+        OwnProductAdapter adapter = new OwnProductAdapter(getBaseContext(), products);
+        listview.setAdapter(adapter);
+
     }
 }
